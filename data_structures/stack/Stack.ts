86ececc1dc31f4e6efb664af9c09fc4760598d70
hdntc <realcirno@protonmail.com>
2022-12-30 12:19:14 +0000

    first commit w/ stack

---
+++ data_structures/stack/Stack.ts
@@ -0,0 +1,18 @@
+class Stack<T> {
+    content: T[];
+    pop(): T {
+        return this.content.pop();
+    };
+    push(top: T): void {
+        this.content.push(top);
+    };
+    constructor(initial: T[] | T | undefined) {
+        if(initial instanceof Array) {
+            this.content = initial;
+        } else if (typeof initial === "undefined") {
+            this.content = [  ];
+        } else {
+            this.content = [ initial ];
+        }
+    };
+};
