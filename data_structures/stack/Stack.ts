5a1746cf76e2b2710be920d08cb829e6773eaf57
hdntc <realcirno@protonmail.com>
2022-12-31 02:30:53 +0000

    add some more tests

--- data_structures/stack/Stack.ts
+++ data_structures/stack/Stack.ts
@@ -1,18 +1,22 @@
 class Stack<T> {
-    content: T[];
+    content: T[] = [];
     pop(): T {
-        return this.content.pop();
+        if(this.content.length === 0) {
+            throw Error("Stack is empty");
+        } else {
+            return this.content.pop();
+        }
     };
     push(top: T): void {
         this.content.push(top);
     };
-    constructor(initial: T[] | T | undefined) {
+    constructor(initial?: T[] | T ) {
         if(initial instanceof Array) {
             this.content = initial;
-        } else if (typeof initial === "undefined") {
-            this.content = [  ];
-        } else {
+        } else if(typeof initial !== "undefined") {
             this.content = [ initial ];
+        } else {
+            this.content = [  ];
         }
     };
 };
--- tests/Stack.test.ts
+++ tests/Stack.test.ts
@@ -1,9 +1,26 @@
-const { Stack } = require("../data_structures/stack/Stack");
+import { Stack } from "../data_structures/stack/Stack";
 
-describe("basic usage", () => {
+describe("basic stack usage", () => {
     it("works with strings", () => {
         const my_stack = new Stack("initial top");
         my_stack.push("hello");
         expect(my_stack.pop()).toBe("hello");
-    })
+    });
+
+    it("works with undefined initial value", () => {
+        const my_stack = new Stack<number>();
+        expect(() => my_stack.pop()).toThrow(Error);
+    });
+
+    it("generates reversed array by repeated popping", () => {
+        const content: any[] = [1, 2, 3, "hi", "hello", 3, 3];
+        let new_array = [];
+        const my_stack = new Stack<any>([1, 2, 3, "hi", "hello", 3, 3]);
+        
+        while(my_stack.content.length > 0) {
+            new_array.push(my_stack.pop());
+        };
+        
+        expect(new_array).toEqual(content.reverse());
+    });
 });
