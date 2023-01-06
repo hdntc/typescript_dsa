e8c4019d83ba060a4429ad751176df4d8b4b2a06
hdntc <realcirno@protonmail.com>
2023-01-06 15:13:19 +0000

    add maxLength test

--- tests/Queue.test.ts
+++ tests/Queue.test.ts
@@ -6,5 +6,11 @@ describe("basic usage", () => {
 
         expect(my_queue.head.value).toBe(5);
         expect(my_queue.rear.value).toBe(5);
-    }); 
+    });
+
+    it("supports maximum length", () => {
+        const my_queue = new Queue<number>([1,2,3,4], 4);
+
+        expect(() => my_queue.enqueue(5)).toThrow(Error("Queue at maximum capacity"));
+    });
 });
