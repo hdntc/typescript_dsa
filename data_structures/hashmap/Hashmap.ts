fded72d64724f6644034a3c33c6ef41b7fbbceed
hdntc <realcirno@protonmail.com>
2023-01-13 14:56:36 +0000

    remove superfluous code

--- data_structures/hashmap/Hashmap.ts
+++ data_structures/hashmap/Hashmap.ts
@@ -1,6 +1,5 @@
 export enum HashmapErrors {
-    INIT_ARRAY_LENGTH_MISMATCH="The array of initial_keys must be the same length as the array of initial_values",
-    INIT_ARRAY_TYPE_MISMATCH="If initial_keys is an array, then initial_values must also be an array"
+    INIT_ARRAY_LENGTH_MISMATCH="The array of initial_keys must be the same length as the array of initial_values"
 };
 
 export class Hashmap<T> {
@@ -25,10 +24,6 @@ export class Hashmap<T> {
     }
     
     constructor(initial_values: T[], initial_keys: string[], hash_function?: (string) => number) {
-        if(!(initial_values instanceof Array)) {
-            throw Error(HashmapErrors.INIT_ARRAY_TYPE_MISMATCH);
-        } 
-        
         if(initial_values.length !== initial_keys.length) {
             throw Error(HashmapErrors.INIT_ARRAY_LENGTH_MISMATCH);
         }
