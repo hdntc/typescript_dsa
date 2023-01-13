06cfc5a9636452d05dc7a867393a1310e43d511f
hdntc <realcirno@protonmail.com>
2023-01-13 14:55:44 +0000

    complete hashmap skeleton code

--- data_structures/hashmap/Hashmap.ts
+++ data_structures/hashmap/Hashmap.ts
@@ -3,14 +3,28 @@ export enum HashmapErrors {
     INIT_ARRAY_TYPE_MISMATCH="If initial_keys is an array, then initial_values must also be an array"
 };
 
-type PossibleInitialKeys = string | string[];
-type PossibleInitialValues<T, IK> = IK extends Array<string> ? T[] | T[][] : T | T[];
-
 export class Hashmap<T> {
+    //currently only supports strings for keys
     values: T[] = [];
     keys: string[] = [];
+    #hash_function: (key: string) => number;
+
+    private _default_hash_function(key: string): number {
+        // return hashed representation of key parameter
+        // the return value is NOT the array index of the value, (return value) % this.values.length is
+        // based on Java's hashCode
+        var hash = 0;
+        var i = 0;
+        var len = key.length;
+
+        while(i < len) {
+            hash = ((hash << 5) - hash + key.charCodeAt(i++)) << 0; // hash <- 31 * hash + (current character code)
+        }
+
+        return hash;
+    }
     
-    constructor(initial_values: T[], initial_keys: string[], hash_function: (string) => number) {
+    constructor(initial_values: T[], initial_keys: string[], hash_function?: (string) => number) {
         if(!(initial_values instanceof Array)) {
             throw Error(HashmapErrors.INIT_ARRAY_TYPE_MISMATCH);
         } 
@@ -21,5 +35,11 @@ export class Hashmap<T> {
         
         this.values = [...initial_values];
         this.keys = [...initial_keys];
+
+        if(typeof hash_function === "undefined") {
+            this.#hash_function = this._default_hash_function;
+        } else {
+            this.#hash_function = hash_function;
+        }
     };
 };
