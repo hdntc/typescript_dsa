e33fecf7668daccc1fc6b19e41a2c10444981644
hdntc <realcirno@protonmail.com>
2023-01-23 01:39:40 +0000

    add dfs

--- data_structures/tree/Tree.ts
+++ data_structures/tree/Tree.ts
@@ -13,6 +13,22 @@ export class TreeNode<T> {
         });
     }
 
+    depth_first_search(value: T, strict: boolean=true): TreeNode<T>[] {
+        let result: TreeNode<T>[] = [];
+
+        if(strict) {
+            if(value === this.value) result.push(this);
+        } else {
+            if(value == this.value) result.push(this);
+        }
+
+        this.#children.forEach(child => {
+            result = [...result, ...child.depth_first_search(value, true)];
+        });
+
+        return result;
+    }
+
     constructor(initial_value: T, children: (TreeNode<T> | T)[]=[]) {
         this.value = initial_value;
         this.children = children;
@@ -21,6 +37,7 @@ export class TreeNode<T> {
 
 export class Tree<T> {
     root: TreeNode<T>;
+    num_nodes: number = 0;
 
     constructor(root: T | TreeNode<T>) {
         if(root instanceof TreeNode) {
@@ -28,5 +45,7 @@ export class Tree<T> {
         } else {
             this.root = new TreeNode<T>(root);
         }
+
+        this.num_nodes = 1 + this.root.children.length;
     }
 };
--- tests/Tree.test.ts
+++ tests/Tree.test.ts
@@ -14,4 +14,25 @@ describe("basic usage", () => {
 
         expect(my_tree.root.children.map(child => child.value)).toEqual([1,3,5,2,5]);
     });
+
+    it("supports dfs (nodes found)", () => {
+        const my_node = new TreeNode<number>(5, [5, 7, new TreeNode(10, [5, 7, 2, 5])]);
+
+        const dfs_result: TreeNode<number>[] = my_node.depth_first_search(5, true);
+
+        expect([
+            my_node === dfs_result[0],
+            my_node.children[0] === dfs_result[1],
+            my_node.children[2].children[0] === dfs_result[2],
+            my_node.children[2].children[3] === dfs_result[3]
+        ].every(x => x===true)).toBe(true);
+    })
+
+    it("supports dfs (node not found)", () => {
+        const my_node = new TreeNode<number>(5);
+
+        const dfs_result: TreeNode<number>[] = my_node.depth_first_search(3);
+
+        expect(dfs_result).toEqual([]);
+    });
 });
