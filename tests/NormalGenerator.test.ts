e69b0ffabe5dbbcd179c26a3839375bd7102f2e8
hdntc <realcirno@protonmail.com>
2023-01-30 20:56:12 +0000

    more config tests

--- tests/NormalGenerator.test.ts
+++ tests/NormalGenerator.test.ts
@@ -23,7 +23,7 @@ describe("basic usage", () => {
     });
 
     it("generates values that are somewhat reasonable wrt config (irwin-hall)", () => {
-        const generator = new NormalGenerator({std: 3, mean: 10, methodID: NormalGeneratorMethod.IRWIN_HALL });
+        const generator = new NormalGenerator({std: 3, mean: 10, methodID: NormalGeneratorMethod.IRWIN_HALL, irwin_hall_samples: 15 });
 
         const values: number[] = generator.generate(10000);
 
@@ -38,5 +38,36 @@ describe("basic usage", () => {
 
     it("throws error w/ invalid std config", () => {
         expect(()=> new NormalGenerator({std: -3, mean: 100, methodID: NormalGeneratorMethod.BOX_MULLER })).toThrow(NormalGeneratorErrors.NONPOSITIVE_STD);
+    });
+
+    it("throws error when supplying invalid # of irwin-hall samples", () => {
+        expect(()=> new NormalGenerator({
+            std: 12, 
+            mean: 10, 
+            methodID: NormalGeneratorMethod.IRWIN_HALL, 
+            irwin_hall_samples: -3 
+        })).toThrow(NormalGeneratorErrors.INVALID_IH_SAMPLES_NUMBER);
+
+        expect(()=> new NormalGenerator({
+            std: 16, 
+            mean: -100, 
+            methodID: NormalGeneratorMethod.IRWIN_HALL, 
+            irwin_hall_samples: 0
+        })).toThrow(NormalGeneratorErrors.INVALID_IH_SAMPLES_NUMBER);
+
+        expect(()=> new NormalGenerator({
+            std: 1, 
+            mean: 0, 
+            methodID: NormalGeneratorMethod.IRWIN_HALL, 
+            irwin_hall_samples: 1.7
+        })).toThrow(NormalGeneratorErrors.INVALID_IH_SAMPLES_NUMBER);
+    });
+
+    it("throws error when supplying # of irwin-hall samples when irwin-hall method is not selected", () => {
+        expect(()=> new NormalGenerator({
+            std: 52, 
+            mean: 100, 
+            irwin_hall_samples: 10
+        })).toThrow(NormalGeneratorErrors.SPECIFIED_SAMPLES_WHEN_NOT_USING_IH);
     });
 });
