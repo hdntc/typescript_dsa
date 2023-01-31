512c9244db6a1b9d1b71ddadb79a04ff28390ce9
hdntc <realcirno@protonmail.com>
2023-01-31 11:12:57 +0000

    Add variance function

--- algorithms/statistics/Essential.ts
+++ algorithms/statistics/Essential.ts
@@ -1,9 +1,36 @@
+/**
+ * Returns the average / mean of the input array.
+ * @param nums The numbers to compute the average of.
+ * @returns The average or mean.
+ */
 const avg = (nums: number[]) => {
     return nums.reduce((acc, curr) => acc + curr) / nums.length;
 }
 
-const std = (nums: number[]) => {
-    return Math.sqrt(nums.map(x => Math.pow(x - avg(nums), 2)).reduce((a, b) => a + b) / nums.length);
+/**
+ * Returns the standard deviation of the input array.
+ * @param nums The numbers to compute the standard deviation of.
+ * @param sample Whether to regard `nums` as a sample or as a population. 
+ * - If `true`, computes the sample std (commonly denoted as *s*). 
+ * - If `false`, computes population std (commonly denoted as *σ*).
+ * - `false` by default.
+ * @returns The standard deviation.
+ */
+const std = (nums: number[], sample: boolean=false) => {
+    return Math.sqrt(vnc(nums, sample));
 }
 
-export { avg, std };
+/**
+ * Returns the variance of the input array.
+ * @param nums The numbers to compute the variance of.
+ * @param sample Whether to regard `nums` as a sample or as a population. 
+ * - If `true`, computes the sample variance (commonly denoted as *s^2*). 
+ * - If `false`, computes population variance (commonly denoted as *σ^2*).
+ * - `false` by default.
+ * @returns The variance.
+ */
+const vnc = (nums: number[], sample: boolean=false) => {
+    return nums.map(x => Math.pow(x - avg(nums), 2)).reduce((a, b) => a + b) / (nums.length - +sample);
+}
+
+export { avg, std, vnc };
