922bd4b939f25a0faf02223d376dbf8dc8398d7d
hdntc <realcirno@protonmail.com>
2023-01-31 10:08:26 +0000

    make generate_one public

--- algorithms/statistics/Generator.ts
+++ algorithms/statistics/Generator.ts
@@ -1,15 +1,15 @@
 export abstract class Generator { 
-    abstract _generate_one(): number
+    abstract generate_one(): number
  
     generate(num_samples: number): number[]
     generate(): number
 
     generate<T extends number>(num_samples?: T): T extends number ? number[] : number  {
         if(typeof num_samples !== "undefined") {
-            return Array(num_samples).fill(undefined).map(_=>this._generate_one()) as any; // apparently the type assertion is needed?
+            return Array(num_samples).fill(undefined).map(_=>this.generate_one()) as any; // apparently the type assertion is needed?
         }
 
-        return this._generate_one() as any;
+        return this.generate_one() as any;
     }
 
     constructor() {
